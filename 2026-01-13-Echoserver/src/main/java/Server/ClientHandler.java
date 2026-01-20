0694b3a21423a6f7b1a6ccc6e027774623822fcb
MileHe210284 <40146720210284@litec.ac.at>
2026-01-20 15:28:06 +0100

    feat:MultiServer

--- 2026-01-13-Echoserver/src/main/java/Server/ClientHandler.java
+++ 2026-01-13-Echoserver/src/main/java/Server/ClientHandler.java
@@ -1,10 +1,21 @@
-package Client;
+package Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable{
     private static int coutner =0;
     private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+
+    }
 
     public ClientHandler(Socket socket){
         coutner++;
@@ -14,6 +25,32 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run() {
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+            String line;
+            while (true) {
+                try {
+                    if (!((line = in.readLine()) != null)) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Received: " + line);
+                out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally{
+            out.close();
+            try {
+                in.close();
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 }
--- 2026-01-13-Echoserver/src/main/java/Server/MultiServer.java
+++ 2026-01-13-Echoserver/src/main/java/Server/MultiServer.java
@@ -1,4 +1,23 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MultiServer {
+    private ServerSocket serverSocket;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        while(true){
+            new Thread(new ClientHandler(serverSocket.accept())).start();
+        }
+    }
+
+    public void stop() throws IOException {
+        serverSocket.close();
+    }
 }
